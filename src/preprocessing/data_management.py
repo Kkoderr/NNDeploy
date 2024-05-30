2a5fd40dd726adfda96d84e27ff61d07acd1c501
Bhavook <hereaslearner@gmail.com>
2024-05-30 12:25:08 +0530

    Added training datasets and made updates in data_management file

--- src/preprocessing/data_management.py
+++ src/preprocessing/data_management.py
@@ -7,3 +7,22 @@ from src.config import config
 def load_datasets(file_name):
 
     file_path = os.path.join(config.DATAPATH, file_name)
+    #"/src/datasets/train.csv"
+
+    data = pd.read_csv(file_path)
+    return data
+
+def save_model(theta0, theta):
+    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH, "\two_input_xor_nn.pkl")
+
+    with open(pkl_file_path, "wb") as file_handle:
+        file_handle.dump({"biases":theta0, "weights":theta})
+
+def load_model(file_name):
+    pkl_file_path = os.path.join(config.SAVED_MODEL_PATH,file_name)
+
+    with open(pkl_file_path,"rb") as file_handle:
+        trained_parameters = file_handle.load()
+
+    return (trained_parameters["biases"], trained_parameters["weights"])
+
